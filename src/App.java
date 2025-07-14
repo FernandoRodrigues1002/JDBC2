3352fc1dbd6157255584763799c96098380641e3
Nando <fernanrods1002@gmail.com>
2025-07-14 16:11:16 -0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,12 @@
+
+import db.DB;
+import java.sql.Connection;
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+        Connection conn = DB.getConnection();
+
+        DB.closeConnection();
+    }
+}
