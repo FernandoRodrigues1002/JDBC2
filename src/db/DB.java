650b3bfc3b364425923280833e45d727c0375b87
Nando <fernanrods1002@gmail.com>
2025-07-14 16:18:26 -0300

    comentario

--- src/db/DB.java
+++ src/db/DB.java
@@ -14,22 +14,21 @@ public class DB {
     public static Connection getConnection() {
         try {
             if (conn == null) {
-                Properties props = loadProperties();
-                String url = props.getProperty("dburl");
-                conn = DriverManager.getConnection(url, props);
+                Properties props = loadProperties(); // carrega configurações
+                String url = props.getProperty("dburl"); // pega a URL do banco
+                conn = DriverManager.getConnection(url, props); // conecta
             }
             return conn;
         } catch (SQLException e) {
-            throw new DbException(e.getMessage());
+            throw new DbException(e.getMessage()); // exceção personalizada
         }
     }
 
     private static Properties loadProperties() {
         try (FileInputStream fs = new FileInputStream("db.properties")) {
             Properties props = new Properties();
-            props.load(fs);
+            props.load(fs); // lê os dados do arquivo
             return props;
-
         } catch (IOException e) {
             throw new DbException(e.getMessage());
         }
