e240ddcefe10bb758bfcf8c855177e204e401e0a
Nando <fernanrods1002@gmail.com>
2025-07-14 17:14:36 -0300

    first commit

--- src/App.java
+++ src/App.java
@@ -1,12 +1,34 @@
 
 import db.DB;
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class App {
 
     public static void main(String[] args) throws Exception {
-        Connection conn = DB.getConnection();
+        Connection conn = null;
+        Statement st = null;
+        ResultSet rs = null;
 
-        DB.closeConnection();
+        try {
+            conn = DB.getConnection();
+
+            st = conn.createStatement();
+
+            rs = st.executeQuery("select * from department");
+
+            while (rs.next()) {
+                System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            DB.closeResultSet(rs);;
+            DB.closeStatment(st);
+            DB.closeConnection();
+        }
+
     }
 }
